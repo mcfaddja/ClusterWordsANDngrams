96869440a9bcd695d02f1656802776c417853ab6
mcfaddja <mcfaddja@uw.edu>
2016-11-13 16:23:30 -0800

    edited code for the n-gram generator for n-grams through size 5 (nGRAMgen5.) to actually run to 5, not 6.

--- MATLAB code/nGRAMgen5.m
+++ MATLAB code/nGRAMgen5.m
@@ -1,4 +1,4 @@
-nVAL = 6;
+nVAL = 5;
 
 alpha = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 alpha = alpha';
