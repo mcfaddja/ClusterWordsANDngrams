1bfbb4de98c42b90e8b808c785a97be88badf577
mcfaddja <mcfaddja@uw.edu>
2016-12-02 22:46:21 -0800

    finished random word list creator.  Working on code for MATLAB function to compile list of DGAs.

---
+++ DGAmaker/wordListMaker.m
@@ -0,0 +1,11 @@
+function [ wordsOut ] = wordListMaker( dictIN )
+%DGAMAKER Summary of this function goes here
+%   Detailed explanation goes here
+
+
+randList = randi(258286, 50, 1);
+wordsOut = dictIN(randList, 1);
+
+
+end
+
